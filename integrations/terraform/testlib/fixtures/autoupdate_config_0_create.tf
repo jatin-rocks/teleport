1a37cf273c207acb408dfed9f41acf0aeb88f2a4
hugoShaka <hugo.hervieux@goteleport.com>
2025-03-19 10:44:00 +0100

    fmt

--- integrations/terraform/testlib/fixtures/autoupdate_config_0_create.tf
+++ integrations/terraform/testlib/fixtures/autoupdate_config_0_create.tf
@@ -5,16 +5,16 @@ resource "teleport_autoupdate_config" "test" {
       mode = "enabled"
     }
     agents = {
-      mode = "enabled"
+      mode     = "enabled"
       strategy = "halt-on-error"
       schedules = {
         regular = [
           # dev is updated at 4:00 UTC
-          {name = "dev", days = ["Mon", "Tue", "Wed", "Thu"], start_hour: 4},
+          { name = "dev", days = ["Mon", "Tue", "Wed", "Thu"], start_hour : 4 },
           # staging is updated at 14:00 UTC
-          {name = "staging", days = ["Mon", "Tue", "Wed", "Thu"], start_hour: 14},
+          { name = "staging", days = ["Mon", "Tue", "Wed", "Thu"], start_hour : 14 },
           # prod is updated at 14:00 UTC the next day
-          {name = "prod", days = ["Mon", "Tue", "Wed", "Thu"], start_hour: 14, wait_hours: 24},
+          { name = "prod", days = ["Mon", "Tue", "Wed", "Thu"], start_hour : 14, wait_hours : 24 },
         ]
       }
     }
--- integrations/terraform/testlib/fixtures/autoupdate_config_1_update.tf
+++ integrations/terraform/testlib/fixtures/autoupdate_config_1_update.tf
@@ -5,17 +5,17 @@ resource "teleport_autoupdate_config" "test" {
       mode = "enabled"
     }
     agents = {
-      mode = "suspended"
-      strategy = "time-based"
+      mode                        = "suspended"
+      strategy                    = "time-based"
       maintenance_window_duration = "45m"
       schedules = {
         regular = [
           # dev is updated at 4:00 UTC
-          {name = "dev", days = ["Mon", "Tue", "Wed", "Thu"], start_hour: 4},
+          { name = "dev", days = ["Mon", "Tue", "Wed", "Thu"], start_hour : 4 },
           # staging is updated at 08:00 UTC
-          {name = "staging", days = ["Mon", "Tue", "Wed", "Thu"], start_hour: 8},
+          { name = "staging", days = ["Mon", "Tue", "Wed", "Thu"], start_hour : 8 },
           # prod is updated at 14:00 UTC
-          {name = "prod", days = ["Mon", "Tue", "Wed", "Thu"], start_hour: 14,},
+          { name = "prod", days = ["Mon", "Tue", "Wed", "Thu"], start_hour : 14, },
         ]
       }
     }
